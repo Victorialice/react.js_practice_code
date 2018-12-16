e5884d4795d96dc11de608dfce94d0ee0533c08a
zhuwenqian <1506675573@qq.com>
2018-12-16 23:41:54 +0800

    add welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+        {[1,2,3]}
+      </div>
+    )
+  }
+}
+export default Welcome
