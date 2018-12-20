e39193acd9059c6407d5da2c84f5646fe93c390d
zhuwenqian <1506675573@qq.com>
2018-12-20 10:34:02 +0800

    add NameCard component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import NameCard from './components/NameCard';
+const tags = ['beautiful', 'wonderful']
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <NameCard name='King' number={1234567890} isHuman tags={tags}/>
       </div>
     );
   }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,18 +2,11 @@ import React from 'react'
 
 class Welcome extends React.Component {
   render() {
-    const todoList = ['Learn React', 'Learn Redux']
     const isLogin = true
+    const test = <h1>hello React</h1>
     return (
       <div className="this" htmlFor="">
       <h1>Hello React</h1>
-      <ul>
-        {
-          todoList.map(item =>
-            <li>{item}</li>
-          )
-        }
-      </ul>
       { isLogin ? <p>你已登录</p> : <p>请登录</p>}
       </div>
     )
