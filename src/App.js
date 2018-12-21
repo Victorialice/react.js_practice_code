7a895632da84d8aa9475a3033819313296bc5b2d
zhuwenqian <1506675573@qq.com>
2018-12-21 10:59:49 +0800

    practice hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import NameCard from './components/NameCard';
 import LikesButton from './components/LikesButton';
+import DigitalClock from './components/DigitalClock';
 const tags = ['beautiful', 'wonderful']
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
       </header>
       <NameCard name='King' number={1234567890} isHuman tags={tags}/>
       <LikesButton />
+      <DigitalClock />
       </div>
     );
   }
